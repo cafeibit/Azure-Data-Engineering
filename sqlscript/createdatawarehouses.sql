f7f433d7c73828e8f95c6b330e5daf6fd4c84301
Cafe Bit <43447794+cafeibit@users.noreply.github.com>
2022-05-06 13:44:57 -0400

    Update createdatawarehouses.sql

--- sqlscript/createdatawarehouses.sql
+++ sqlscript/createdatawarehouses.sql
@@ -20,3 +20,36 @@ ORDER BY Region;
 --When we use PARTITION BY with the OVER clause (1), we divide the query result set into partitions. 
 --The window function is applied to each partition separately and computation restarts for each partition.
 
+--To build window functions that use the PERCENTILE_CONT and PERCENTILE_DISC functions.
+
+-- PERCENTILE_CONT, PERCENTILE_DISC
+SELECT DISTINCT c.Category  
+,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY bc.Downloads)
+                    OVER (PARTITION BY Category) AS MedianCont  
+,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY bc.Downloads)
+                    OVER (PARTITION BY Category) AS MedianDisc  
+FROM dbo.Category AS c  
+INNER JOIN dbo.BookList AS bl
+    ON bl.CategoryID = c.ID
+INNER JOIN dbo.BookConsumption AS bc  
+    ON bc.BookID = bl.ID
+ORDER BY Category
+
+--LAG Function
+SELECT ProductId,
+    [Hour] AS SalesHour,
+    TotalAmount AS CurrentSalesTotal,
+    LAG(TotalAmount, 1,0) OVER (ORDER BY [Hour]) AS PreviousSalesTotal,
+    TotalAmount - LAG(TotalAmount,1,0) OVER (ORDER BY [Hour]) AS Diff
+FROM [wwi_perf].[Sale_Index]
+WHERE ProductId = 3848 AND [Hour] BETWEEN 8 AND 20;
+
+-- ROWS UNBOUNDED PRECEDING
+SELECT DISTINCT bc.Country, b.Title AS Book, bc.Downloads
+    ,FIRST_VALUE(b.Title) OVER (PARTITION BY Country  
+        ORDER BY Downloads ASC ROWS UNBOUNDED PRECEDING) AS FewestDownloads
+FROM dbo.BookConsumption AS bc
+INNER JOIN dbo.Books AS b
+    ON b.ID = bc.BookID
+ORDER BY Country, Downloads
+
