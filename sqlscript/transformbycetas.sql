1bdf9af867e4da0111214c23ae89559dbcdf849d
Cafe Bit <43447794+cafeibit@users.noreply.github.com>
2022-05-09 12:55:24 -0400

    Update transformbycetas.sql

--- sqlscript/transformbycetas.sql
+++ sqlscript/transformbycetas.sql
@@ -59,3 +59,27 @@ GO
 
 -- you can query the newly created external table
 SELECT * FROM aggregated_data
+
+--
+--Operationalize data transformation with Azure Synapse serverless SQL pools using stored procedures
+--
+
+-- this sample references external data source and external file format defined in previous section
+CREATE PROCEDURE usp_calculate_population_by_year_state
+AS
+BEGIN
+	CREATE EXTERNAL TABLE population_by_year_state
+	WITH (
+		LOCATION = 'population_by_year_state/',
+		DATA_SOURCE = destination_ds,  
+		FILE_FORMAT = parquet_file_format
+	)  
+	AS
+	SELECT decennialTime, stateName, SUM(population) AS population
+	FROM
+		OPENROWSET(BULK 'https://azureopendatastorage.blob.core.windows.net/censusdatacontainer/release/us_population_county/year=*/*.parquet',
+		FORMAT='PARQUET') AS [r]
+	GROUP BY decennialTime, stateName
+END
+GO
+
