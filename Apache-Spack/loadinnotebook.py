df89bb6e5b9789d21556135db79b024c313bef76
Cafe Bit <43447794+cafeibit@users.noreply.github.com>
2022-05-10 15:24:14 -0400

    Update loadinnotebook.py

--- Apache-Spack/loadinnotebook.py
+++ Apache-Spack/loadinnotebook.py
@@ -119,3 +119,78 @@ profitByDateProduct = (data_path.groupBy("TransactionDate","ProductId")
     .orderBy("TransactionDate"))
 display(profitByDateProduct.limit(100))
 
+
+#Exercise: Flatten nested structures and explode arrays with Apache Spark in synapse
+#In this exercise, you will learn how to work with complex data structure and use functions to view data more easily
+
+#1. PySpark contains a special explode function, which returns a new row for each element of the array. 
+#The new row will help flatten the topProductPurchases column for better readability or for easier querying. 
+#Execute the code below in a new cell:
+
+from pyspark.sql.functions import udf, explode
+
+flat=df.select('visitorId',explode('topProductPurchases').alias('topProductPurchases_flat'))
+flat.show(100)
+
+#In this cell, we created a new DataFrame named flat that includes the visitorId field and a new aliased field named topProductPurchases_flat. 
+#As you can see, the output is a bit easier to read and, by extension, easier to query.
+
+#2. Create a new cell and execute the following code to create a new flattened version of the DataFrame that extracts 
+#the topProductPurchases_flat.productId and topProductPurchases_flat.itemsPurchasedLast12Months fields to create new rows for each data combination:
+
+topPurchases = (flat.select('visitorId','topProductPurchases_flat.productId','topProductPurchases_flat.itemsPurchasedLast12Months')
+    .orderBy('visitorId'))
+
+topPurchases.show(100)
+
+#Note: In the output, notice that we now have multiple rows for each visitorId.
+
+#3. Let's order the rows by the number of items purchased in the last 12 months. Create a new cell and execute the following code:
+
+# Let's order by the number of items purchased in the last 12 months
+sortedTopPurchases = topPurchases.orderBy("itemsPurchasedLast12Months")
+
+display(sortedTopPurchases.limit(100))
+
+#4. How do we sort in reverse order? You might conclude that we could make a call like this:
+#topPurchases.orderBy("itemsPurchasedLast12Months desc")
+#Notice that there is an AnalysisException error, because itemsPurchasedLast12Months desc does not match up with a column name.
+
+#5. The Column class is an object that encompasses more than just the name of the column, 
+#but also column-level-transformations, such as sorting in a descending order. Execute the following code in a new cell:
+
+sortedTopPurchases = (topPurchases
+    .orderBy( col("itemsPurchasedLast12Months").desc() ))
+
+display(sortedTopPurchases.limit(100))
+
+#Notice that the results are now sorted by the itemsPurchasedLast12Months column in descending order, 
+#thanks to the desc() method on the col object.
+
+#6. How many types of products did each customer purchase? To find the answer, we need to group by visitorId and aggregate on the number of rows per customer. 
+#Execute the following code in a new cell:
+
+groupedTopPurchases = (sortedTopPurchases.select("visitorId")
+    .groupBy("visitorId")
+    .agg(count("*").alias("total"))
+    .orderBy("visitorId") )
+
+display(groupedTopPurchases.limit(100))
+
+#Notice how we use the groupBy method on the visitorId column, and the agg method over a count of records to display the total for each customer.
+
+#7. How many total items did each customer purchase? To find the answer, we need to group by visitorId 
+#and aggregate on the sum of itemsPurchasedLast12Months values per customer. 
+#Execute the following code in a new cell:
+
+groupedTopPurchases = (sortedTopPurchases.select("visitorId","itemsPurchasedLast12Months")
+    .groupBy("visitorId")
+    .agg(sum("itemsPurchasedLast12Months").alias("totalItemsPurchased"))
+    .orderBy("visitorId") )
+
+groupedTopPurchases.show(100)
+
+#Here we group by visitorId once again, but now we use a sum over the itemsPurchasedLast12Months column in the agg method. 
+#Notice that we included the itemsPurchasedLast12Months column in the select statement so we could use it in the sum.
+
+
